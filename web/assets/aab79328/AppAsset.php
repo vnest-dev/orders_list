62520018ff74886cdfce95f88eea4277515c0a3d
vnest-dev <vladimir.nesterenko.workmail@gmail.com>
2021-02-10 14:47:29 +0300

    ассеты

---
+++ web/assets/aab79328/AppAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace orders\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class AppAsset
+ * Modules asset class
+ *
+ * @package orders\assets
+ */
+class AppAsset extends AssetBundle
+{
+    public $sourcePath = '@app/modules/orders/assets';
+    public $css = [
+        'css/custom.css',
+        'css/bootstrap.min.css'
+    ];
+    public $js = [
+        'js/jquery.min.js',
+        'js/bootstrap.min.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
